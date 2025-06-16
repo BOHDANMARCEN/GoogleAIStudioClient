6a72de22b1768da2f86b14f47471634d772e2294
Bogdan Martseniuk <bodykabest@gmail.com>
2025-06-16 04:40:47 +0300

    Configure plugin management and kapt

--- GoogleAIStudioClient/settings.gradle.kts
+++ GoogleAIStudioClient/settings.gradle.kts
@@ -1,4 +1,12 @@
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "GoogleAIStudioClient"
 include(":app")
 
